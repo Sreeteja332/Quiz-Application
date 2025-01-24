66e972ef68c66934dba146099d83227e4f5e041c
Sreeteja332 <sreeteja332@gmail.com>
2025-01-24 17:31:15 +0530

    final commit

--- src/main/java/com/quizapp/Quiz.java
+++ src/main/java/com/quizapp/Quiz.java
@@ -49,76 +49,76 @@ public class Quiz extends JFrame {
         question.setFont(new Font("Tahoma", Font.PLAIN, 24));
         add(question);
 
-        questions[0][0] = "Which is used to find and fix bugs in the Java programs.?";
-        questions[0][1] = "JVM";
-        questions[0][2] = "JDB";
-        questions[0][3] = "JDK";
-        questions[0][4] = "JRE";
+        questions[0][0] = "What color is a banana?";
+        questions[0][1] = "Red";
+        questions[0][2] = "Yellow";
+        questions[0][3] = "Blue";
+        questions[0][4] = "Green";
 
-        questions[1][0] = "What is the return type of the hashCode() method in the Object class?";
-        questions[1][1] = "int";
-        questions[1][2] = "Object";
-        questions[1][3] = "long";
-        questions[1][4] = "void";
+        questions[1][0] = "How many legs does a dog have?";
+        questions[1][1] = "3";
+        questions[1][2] = "4";
+        questions[1][3] = "2";
+        questions[1][4] = "5";
 
-        questions[2][0] = "Which package contains the Random class?";
-        questions[2][1] = "java.util package";
-        questions[2][2] = "java.lang package";
-        questions[2][3] = "java.awt package";
-        questions[2][4] = "java.io package";
+        questions[2][0] = "What is the first letter of the alphabet?";
+        questions[2][1] = "B";
+        questions[2][2] = "C";
+        questions[2][3] = "A";
+        questions[2][4] = "D";
 
-        questions[3][0] = "An interface with no fields or methods is known as?";
-        questions[3][1] = "Runnable Interface";
-        questions[3][2] = "Abstract Interface";
-        questions[3][3] = "Marker Interface";
-        questions[3][4] = "CharSequence Interface";
+        questions[3][0] = "Which animal says 'Meow'?";
+        questions[3][1] = "Dog";
+        questions[3][2] = "Cat";
+        questions[3][3] = "Horse";
+        questions[3][4] = "Cow";
 
-        questions[4][0] = "In which memory a String is stored, when we create a string using new operator?";
-        questions[4][1] = "Stack";
-        questions[4][2] = "String memory";
-        questions[4][3] = "Random storage space";
-        questions[4][4] = "Heap memory";
+        questions[4][0] = "What is 2 + 2?";
+        questions[4][1] = "3";
+        questions[4][2] = "5";
+        questions[4][3] = "4";
+        questions[4][4] = "6";
 
-        questions[5][0] = "Which of the following is a marker interface?";
-        questions[5][1] = "Runnable interface";
-        questions[5][2] = "Remote interface";
-        questions[5][3] = "Readable interface";
-        questions[5][4] = "Result interface";
+        questions[5][0] = "What color is the sky on a clear day?";
+        questions[5][1] = "Green";
+        questions[5][2] = "Red";
+        questions[5][3] = "Blue";
+        questions[5][4] = "Yellow";
 
-        questions[6][0] = "Which keyword is used for accessing the features of a package?";
-        questions[6][1] = "import";
-        questions[6][2] = "package";
-        questions[6][3] = "extends";
-        questions[6][4] = "export";
+        questions[6][0] = "How many days are in a week?";
+        questions[6][1] = "5";
+        questions[6][2] = "6";
+        questions[6][3] = "7";
+        questions[6][4] = "8";
 
-        questions[7][0] = "In java, jar stands for?";
-        questions[7][1] = "Java Archive Runner";
-        questions[7][2] = "Java Archive";
-        questions[7][3] = "Java Application Resource";
-        questions[7][4] = "Java Application Runner";
+        questions[7][0] = "What is the capital of the United States?";
+        questions[7][1] = "New York";
+        questions[7][2] = "Los Angeles";
+        questions[7][3] = "Washington D.C.";
+        questions[7][4] = "Chicago";
 
-        questions[8][0] = "Which of the following is a mutable class in java?";
-        questions[8][1] = "java.lang.StringBuilder";
-        questions[8][2] = "java.lang.Short";
-        questions[8][3] = "java.lang.Byte";
-        questions[8][4] = "java.lang.String";
+        questions[8][0] = "Which fruit is red and round?";
+        questions[8][1] = "Banana";
+        questions[8][2] = "Apple";
+        questions[8][3] = "Grape";
+        questions[8][4] = "Orange";
 
-        questions[9][0] = "Which of the following option leads to the portability and security of Java?";
-        questions[9][1] = "Bytecode is executed by JVM";
-        questions[9][2] = "The applet makes the Java code secure and portable";
-        questions[9][3] = "Use of exception handling";
-        questions[9][4] = "Dynamic binding between objects";
+        questions[9][0] = "What is the largest animal?";
+        questions[9][1] = "Cat";
+        questions[9][2] = "Dog";
+        questions[9][3] = "Elephant";
+        questions[9][4] = "Mouse";
 
-        answers[0][1] = "JDB";
-        answers[1][1] = "int";
-        answers[2][1] = "java.util package";
-        answers[3][1] = "Marker Interface";
-        answers[4][1] = "Heap memory";
-        answers[5][1] = "Remote interface";
-        answers[6][1] = "import";
-        answers[7][1] = "Java Archive";
-        answers[8][1] = "java.lang.StringBuilder";
-        answers[9][1] = "Bytecode is executed by JVM";
+        answers[0][1] = "Yellow";
+        answers[1][1] = "4";
+        answers[2][1] = "A";
+        answers[3][1] = "Cat";
+        answers[4][1] = "4";
+        answers[5][1] = "Blue";
+        answers[6][1] = "7";
+        answers[7][1] = "Washington D.C.";
+        answers[8][1] = "Apple";
+        answers[9][1] = "Elephant";
 
         opt1 = new JRadioButton();
         opt1.setBounds(170, 420, 700, 30);
